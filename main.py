d1207005d94ce5ec6d63217ef2aa2c9be21d6c48
JoanKenobi <joan1233blanco@gmail.com>
2021-12-14 18:06:57 -0400

    init-subiendo mi primera version de el simulador

---
+++ main.py
@@ -0,0 +1,8 @@
+from tools import crear_tabla, graficar_tabla, caracterizar
+
+tabla1=crear_tabla(0,100,100)
+#print(tabla1)
+grafica=graficar_tabla(tabla1)
+#print(grafica)
+valor=caracterizar(tabla1, 15, 50)
+print(valor)
